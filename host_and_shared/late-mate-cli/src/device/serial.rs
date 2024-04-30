e0817a584558a66a2fbfe7511824a508e12ede52
Dan Groshev <git@dgroshev.com>
2024-04-30 14:06:03 +0100

    a hack to make serial port finding work on raspberry flavoured linux

--- host_and_shared/late-mate-cli/src/device/serial.rs
+++ host_and_shared/late-mate-cli/src/device/serial.rs
@@ -7,9 +7,19 @@ pub fn find_serial_port() -> anyhow::Result<SerialPortInfo> {
         .context("Serial port enumeration error")?
         .into_iter()
         .find(|info| {
+            // todo: this is, again, a hack (serial terminal doesn't tell me about itself on raspberry??)
+            if info.port_name == "/dev/ttyACM0" {
+                println!("found /dev/ttyACM0, shortcircuiting (A HACK)");
+                return true;
+            }
+
             if let SerialPortType::UsbPort(UsbPortInfo { vid, pid, .. }) = &info.port_type {
                 // todo: that starts_with is probably incorrect on Windows
-                *vid == USB_VID && *pid == USB_PID && info.port_name.starts_with("/dev/cu.")
+                *vid == USB_VID
+                    && *pid == USB_PID
+                    && (info.port_name.starts_with("/dev/cu.")
+                        // todo: this a hack to find the right serial terminal on linux
+                        || info.port_name.starts_with("/dev/ttyACM"))
             } else {
                 false
             }
