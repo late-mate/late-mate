174abf6bb3df4836448c20653cb1e5c7dc724313
Dan Groshev <git@dgroshev.com>
2024-07-01 19:14:58 +0100

    align paths properly

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite';
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
+  // Note that this is important since Zola copies the entire /static
+  // into the final result, and I need to separate "built" content, so
+  // into the built/ subdir it goes and in-CSS urls must be adjusted
+  // accordingly
+  base: '/built/',
+
   build: {
     manifest: true,
     outDir: 'static/built',
+    assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
       input: './src/main.ts',
